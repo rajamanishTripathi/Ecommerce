26580ecc6c66a44107ff70c3ba4b55b814f57288
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-11 08:49:13 +0530

    created store model

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True) 
+    # for first time when product add time will update with auto_now_add
+    # so used auto_now
+
+
 # Create your models here.

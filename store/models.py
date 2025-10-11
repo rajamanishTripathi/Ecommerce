d12b5801e6098bbeafdcd96c0eafa7f3079ee33d
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-11 09:41:13 +0530

    creaded order and customer models

--- store/models.py
+++ store/models.py
@@ -9,5 +9,33 @@ class Product(models.Model):
     # for first time when product add time will update with auto_now_add
     # so used auto_now
 
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
 
-# Create your models here.
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        ('P','Pending'),
+        ('C','Complete'),
+        ('F','Failed'),
+    ]
+    placed_at = models.DateField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1,choices=PAYMENT_STATUS_CHOICES)
+

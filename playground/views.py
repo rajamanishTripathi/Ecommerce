0214739aed4bf18bee8811c5af7b5f095aaa12cb
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-15 09:44:38 +0530

    complex Q objects for OR ,AND opertions

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from store.models import Product
 
 
@@ -9,6 +10,11 @@ def say_hello(request):
     # except ObjectDoesNotExist:
     #     pass
     # queryset = Product.objects.filter(unit_price__range =(1,20))
-    queryset = Product.objects.filter(title__icontains ='coffee')
-    
-    return render(request, 'hello.html', {'name': 'Mosh','product':queryset})
+    # queryset = Product.objects.filter(title__icontains ='coffee')
+    # queryset = Product.objects.filter(inventory__lt =10,unit_price__lt = 10) AND operation
+    # queryset = Product.objects.filter(inventory__lt =10).filter(unit_price__lt = 20) AND operations
+    # queryset = Product.objects.filter(Q(inventory__lt =10) | Q(unit_price__lt = 20))  OR opertions
+    # queryset = Product.objects.filter(Q(inventory__lt =10) & Q(unit_price__lt = 20))  AND opertion
+    #  queryset = Product.objects.filter(Q(inventory__lt =10) | ~Q(unit_price__lt = 20))  OR opertions with NOT operator
+
+    return render(request, 'hello.html', {'name': 'Mosh','product':list(queryset)})

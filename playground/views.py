dbd51f72187519b685395d98ee4c0677601cb869
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-15 09:33:25 +0530

    query set examples

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    # try:
+    #     product = Product.objects.get(pk=0)
+    # except ObjectDoesNotExist:
+    #     pass
+    # queryset = Product.objects.filter(unit_price__range =(1,20))
+    queryset = Product.objects.filter(title__icontains ='coffee')
+    
+    return render(request, 'hello.html', {'name': 'Mosh','product':queryset})

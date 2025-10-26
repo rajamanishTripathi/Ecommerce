fe4bd79263e2fbad5038ecbb9199e7b28e9600d6
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-26 16:11:37 +0530

    deleting objects

--- playground/views.py
+++ playground/views.py
@@ -2,16 +2,9 @@ from django.shortcuts import render
 from store.models import Product,Order,OrderItem,Customer,Collection
 
 def say_hello(request):
-    # collection = Collection(pk=11)
-    # collection.title = 'Game'
-    # collection.featured_product = None
-    # # or Product(id=1)
-    # collection.save()
-
-    #      ------------------OR-------------------
-    # this get the data first and then update only required fields not all fields
-    # by filtering a id first otherwise it will update all fiels
-    Collection.objects.filter(pk=11).update(featured_product_id=None)
+    collection = Collection(pk=11)
+    # collection.delete()
+    collection.objects.filter(id__gt=5).delete()  
 
 
     return render(request, 'hello.html', {'name': 'Mosh'})

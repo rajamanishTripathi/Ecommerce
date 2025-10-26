b917536e14a724ad25e7bc590c99d0aa005f62a3
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-26 12:25:08 +0530

    getting tags for product

--- playground/views.py
+++ playground/views.py
@@ -1,70 +1,16 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from django.db.models.aggregates import Max, Count, Min, Avg, Sum
-from django.db.models import Q, F, Func, Count, ExpressionWrapper, DecimalField
-from django.db.models import Value
-from django.db.models.functions import Concat
-from store.models import Product, OrderItem, Order, Customer
-
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 def say_hello(request):
-    # try:
-    #     product = Product.objects.get(pk=0)
-    # except ObjectDoesNotExist:
-    #     pass
-    # queryset = Product.objects.filter(unit_price__range =(1,20))
-    # queryset = Product.objects.filter(title__icontains ='coffee')
-    # queryset = Product.objects.filter(inventory__lt =10,unit_price__lt = 10) AND operation
-    # queryset = Product.objects.filter(inventory__lt =10).filter(unit_price__lt = 20) AND operations
-    # queryset = Product.objects.filter(Q(inventory__lt =10) | Q(unit_price__lt = 20))  OR opertions
-    # queryset = Product.objects.filter(Q(inventory__lt =10) & Q(unit_price__lt = 20))  AND opertion
-    #  queryset = Product.objects.filter(Q(inventory__lt =10) | ~Q(unit_price__lt = 20))  OR opertions with NOT operator
-    # Product : inventory = unit_price
-    # queryset = Product.objects.filter(inventory=F('unit_price')) 
-    # queryset = Product.objects.order_by('title')  ASC
-    # queryset = Product.objects.order_by('-title')  DESC
-    # queryset = Product.objects.order_by('unit_price', '-title').reverse()
-    # product = Product.objects.order_by('unit_price')[0]
-    #  product = Product.objects.earliest('unit_price') asc
-    # product = Product.objects.latest('unit_price')[0] desc
-    # product = Product.objects.values('id','title','collection__title')
-    #  instead of product instances we get dictonary object 
-    # product = Product.objects.values_list('id','title','collection__title')
-    #  instead of product instances we get tuple object 
-    # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    content_type = ContentType.objects.get_for_model(Product)
 
-    # queryset = Product.objects.only('id','title') costly queryset
-    # queryset = Product.objects.defer('description')  it will defer loading of certain query
-    # queryset = Product.objects.all()   loading  takes time
-    # queryset = Product.objects.select_related('collection').all()  
-    # this will preload so takes less time. instead of 1000 query there is 3 query
-    # select_related if product has (1) collection
-    # queryset = Product.objects.select_related('collection_someOtherField').all()
-    # prefetch_related if other end relationship has many objects (n)
-    # queryset = Product.objects.prefetch_related('promotions').all()
-    # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
-    # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[0:5]
-    # orderitem_set convention for reverse relation to order table
-    # get last five orders with customer and products included
-
-    # result = Product.objects.aggregate(Count('id')) 
-    # result = Product.objects.aggregate(count = Count('id'), minPrice = Min('unit_price'))
-    # result = Product.objects.filter(collection__id= 1).aggregate(count = Count('id'), minPrice = Min('unit_price'))
-    # queryset = Customer.objects.annotate(is_new=Value(True))
-    # queryset = Customer.objects.annotate(new_id=F('id')+1)
-    # annotate => in order to add additional attributes while querying
-    # queryset = Customer.objects.annotate(
-    #     #concat
-    #     full_name = Func(F('first_name'),Value('  '),F('last_name'), function='CONCAT')
-    # )
-
-    # queryset = Customer.objects.annotate(
-    #     order_count = Count('order')
-    # )
-    # reverse relationship is not working for customer that is order_set. so order entered
-    discounted_price = ExpressionWrapper(F('unit_price')*0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price=discounted_price
-    )
+    queryset = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id = 1
+        )
 
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})

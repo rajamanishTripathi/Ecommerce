a5548e273cbc42adcd242898b3f626b9a29c12b1
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-26 17:01:08 +0530

    raw query execution

--- playground/views.py
+++ playground/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render
 from store.models import Product,Order,OrderItem,Customer,Collection
-from django.db import transaction
+from django.db import transaction,connection
 
 def say_hello(request):
 
-    # ...some code
+    queryset = Product.objects.raw('SELECT * FROM store_product')
 
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
+    # cursor = connection.cursor()
+    # cursor.execute('SELECT * FROM store_product')
+    # cursor.close()
 
-        item = OrderItem()
-        item.order = order
-        item.product_id = -1
-        item.quantity = 1
-        item.unit_price = 1
-        item.save() 
+    # with connection.cursor() as cursor:
+    #     cursor.execute()
 
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers',[1,2,'a'])
 
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    return render(request, 'hello.html', {'name': 'Mosh','query' : queryset})

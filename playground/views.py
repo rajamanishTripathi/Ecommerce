40199edfad66b5b3503b2bfcd91f5a210f0adbec
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-26 14:47:17 +0530

    updating objects

--- playground/views.py
+++ playground/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from store.models import Product,Order,OrderItem,Customer,Collection
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = 'Video Game'
-    collection.featured_product = Product(pk=1) 
-    # or Product(id=1)
-    collection.save()
+    # collection = Collection(pk=11)
+    # collection.title = 'Game'
+    # collection.featured_product = None
+    # # or Product(id=1)
+    # collection.save()
 
     #      ------------------OR-------------------
-    # collection = Collection.objects.create(title='a',featured_product_id=1)
+    # this get the data first and then update only required fields not all fields
+    # by filtering a id first otherwise it will update all fiels
+    Collection.objects.filter(pk=11).update(featured_product_id=None)
 
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh'})

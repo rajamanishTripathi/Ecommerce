6b9d654768824ca068ad4113272100762a75ef29
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-29 08:56:26 +0530

    hyperlink custom serializering relationships

--- store/serializers.py
+++ store/serializers.py
@@ -15,7 +15,11 @@ class ProductSerializer(serializers.Serializer):
     #     queryset = Collection.objects.all()
     # )
     # collection = serializers.StringRelatedField()
-    collection = CollectionSerializer()
+    # collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset = Collection.objects.all(),
+        view_name = 'collection-detail'
+    )
 
     def calculate_tax(self, product:Product):
         return product.unit_price* Decimal(1.1)

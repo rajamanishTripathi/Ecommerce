4dbe5004f7e725ee30d51f446f85f7957e644bcd
Raja Manish <raghupatiraghav882@gmail.com>
2025-10-29 07:53:42 +0530

    creating serializers and object of it

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view()
@@ -10,4 +13,9 @@ def product_list(request):
 
 @api_view()
 def product_detail(request,id):
-   return Response(id)
+    try:
+            product = Product.objects.get(pk=id)
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)

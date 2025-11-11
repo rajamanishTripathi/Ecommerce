7ae71c6aa52a7218d248fbc11bc01ffdbd7e14da
Raja Manish <raghupatiraghav882@gmail.com>
2025-11-11 18:25:18 +0530

    removing comments and implementing generic view

--- store/serializers.py
+++ store/serializers.py
@@ -3,44 +3,18 @@ from .models import Product,Collection
 from decimal import Decimal
 
 
-class CollectionSerializer(serializers.Serializer):
+class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = ['id','title','products_count']
 
-    products_count  = serializers.IntegerField()
+    products_count  = serializers.IntegerField(read_only=True)
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id','title','description','slug','inventory','unit_price','collection']
-        # fields ='__all__'     all fields exposed via api
-    # price = serializers.DecimalField(max_digits=6,decimal_places=2, source='unit_price')
-#     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-#     # collection = serializers.PrimaryKeyRelatedField(
-#     #     queryset = Collection.objects.all()
-#     # )
-#     # collection = serializers.StringRelatedField()
-#     # collection = CollectionSerializer()
-#     collection = serializers.HyperlinkedRelatedField(
-#         queryset = Collection.objects.all(),
-#         view_name = 'collection-detail'
-#     )
 
     def calculate_tax(self, product:Product):
         return product.unit_price* Decimal(1.1)
-
-# #   overriding create method
-#     def create(self, validated_data):
-#         # unpacking validated data
-#         product = Product(**validated_data)
-#         product.other = 1
-#         product.save()
-#         return product
-    
-#     # overring update method
-#     def update(self, instance, validated_data):
-#         instance.unit_price = validated_data.get('unit_price')
-#         instance.save()
-#         return instance
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
@@ -9,17 +10,20 @@ from django.db.models import Count
 from .serializers import ProductSerializer,CollectionSerializer,Collection
 
 
-class ProductList(APIView):
-    def get(self,request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    # implementing below if no logic  required
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
+
+    # implementing below if more logic  required
+    # def get_queryset(self):
+    #     return Product.objects.select_related('collection').all()
     
-    def post(self, request):
-        serializer = ProductSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # def get_serializer_class(self):
+    #     return ProductSerializer
+    
+    def get_serializer_context(self):
+        return {'request':self.request}
     
 class ProductDetail(APIView):
     def get(self,request,id):
@@ -39,18 +43,12 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
         
+
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    print(queryset)
+    serializer_class = CollectionSerializer
     
-@api_view(['GET','POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(products_count=Count('products')).all
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET','PUT','DELETE'])
 def collection_detail(request,pk):

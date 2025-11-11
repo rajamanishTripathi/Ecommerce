f55f998b8a3d9367465adf65ef441acbd755ace6
Raja Manish <raghupatiraghav882@gmail.com>
2025-11-11 19:57:54 +0530

    viewset and routers implementation

--- store/views.py
+++ store/views.py
@@ -4,31 +4,18 @@ from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from .models import Product
 from django.db.models import Count
 from .serializers import ProductSerializer,CollectionSerializer,Collection
 
-
-class ProductList(ListCreateAPIView):
-    # implementing below if no logic  required
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
-
-    # implementing below if more logic  required
-    # def get_queryset(self):
-    #     return Product.objects.select_related('collection').all()
-    
-    # def get_serializer_class(self):
-    #     return ProductSerializer
-    
-    def get_serializer_context(self):
-        return {'request':self.request}
-    
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # lookup_field = 'id'
+    def get_serializer_context(self):
+        return {'request':self.request}
+
     def delete(self,request,pk):
         product = get_object_or_404(Product,pk=pk)
         if product.orderitem_set.count() > 0:
@@ -36,15 +23,8 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
         
-
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    print(queryset)
-    serializer_class = CollectionSerializer
-    
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count = Count('products'))
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):

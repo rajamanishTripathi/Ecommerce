c76f1eda7280615ecdaafaf84518f4065eb8ea48
Raja Manish <raghupatiraghav882@gmail.com>
2025-11-14 13:04:36 +0530

    created cart api endpoint with post req

--- store/serializers.py
+++ store/serializers.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from store.models import Product, Collection, Review
+from .models import Product, Collection, Review, Cart, CartItem
 from rest_framework import serializers
 
 
@@ -31,3 +31,10 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id = self.context['product_id']
         return Review.objects.create(product_id=product_id, **validated_data)
+    
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only = True)
+    class Meta:
+        model = Cart
+        # fields = ['id', 'created_at'] as created_at (for maintainence) no need to returning it to client end so not need to send it to server
+        fields = ['id']
--- store/views.py
+++ store/views.py
@@ -4,11 +4,12 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework import status
+from rest_framework.mixins import CreateModelMixin
 from .filters import ProductFilter
-from .models import Collection, Product, Review
-from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
+from .models import Collection, Product, Review, Cart
+from .serializers import CartSerializer, CollectionSerializer, ProductSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -52,3 +53,7 @@ class ReviewViewSet(ModelViewSet):
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
+    
+class CartViewSet(CreateModelMixin, GenericViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
